bb1ed0bcfcb69b8296087262f77465f20d3e110f
Sami Kallinen <notjustsilicon@gmail.com>
2020-10-14 12:43:39 +0300

    project base

---
+++ App.tsx
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+declare const global: {HermesInternal: null | {}};
+
+const App = () => {
+  return (
+    <>
+      <SafeAreaView>
+        <View>
+          <Text>Here!</Text>
+        </View>
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
